935cf775b0eaa3e286a1ef34aa7762462bcb970a
HinaChoudhry <hinachoudhry@msn.com>
2021-09-03 08:03:33 +0000

    Initial commit

---
+++ pawfect_pets/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pawfect_pets project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pawfect_pets.settings')
+
+application = get_asgi_application()
