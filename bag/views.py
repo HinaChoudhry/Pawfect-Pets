d5cde433415dcdb07ce7369ca9b20cd97e5f6ab6
HinaChoudhry <hinachoudhry@msn.com>
2021-09-08 11:13:50 +0000

    Added toast css with shopping bag preview and additional messages

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -13,13 +13,14 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to bag')
@@ -32,13 +33,16 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """ Adjust the quantity of the specific product to the specified amount """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -50,9 +54,11 @@ def adjust_bag(request, item_id):
 def remove_from_bag(request, item_id):
     """ Remove items from shopping bag"""
 
+    product = get_object_or_404(Product, pk=item_id)
     bag = request.session.get('bag', {})
 
     bag.pop(item_id)
+    messages.success(request, f'Removed {product.name} from bag')
 
     request.session['bag'] = bag
     return HttpResponse(status=200)
