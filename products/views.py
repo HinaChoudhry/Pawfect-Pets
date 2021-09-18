689040a75aa3d94ea3a7dd78a23a7344d8f0b7a6
HinaChoudhry <hinachoudhry@msn.com>
2021-09-18 09:58:10 +0000

    Added product view, url and template with add products form

--- products/views.py
+++ products/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Product, Category
-# Create your views here.
+from .forms import ProductForm
+
 
 def all_products(request):
     """ Shows all products, including sorting and search queries """
@@ -68,3 +70,14 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
