869d10690051e988766e6c04936bb61e557becec
HinaChoudhry <hinachoudhry@msn.com>
2021-09-20 09:08:53 +0000

    Added to the admin.py file to enable reviews from the admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+
+class ReviewInline(admin.TabularInline):
+    """ Allows view/edit of reviews from Product detail page """
+    model = Review
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'product',
+        'user',
+        'date',
+    )
+
+
