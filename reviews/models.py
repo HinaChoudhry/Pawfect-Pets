a772db834d286c12201fa804100f94f721544ae4
HinaChoudhry <hinachoudhry@msn.com>
2021-09-20 08:56:05 +0000

    Create the reviews app and model

---
+++ reviews/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from profiles.models import UserProfile
+from products.models import Product
+
+
+class Review(models.Model):
+    """ 
+    Creates a reviews model
+    """
+
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    comment = models.TextField(max_length=1000, null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
