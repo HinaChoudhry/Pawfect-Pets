c68407300b7badf96af46540f4d24704d24d7eb0
HinaChoudhry <hinachoudhry@msn.com>
2021-09-20 11:53:45 +0000

    Bug fix - form submission not working due to mistyped template in views.py

--- reviews/views.py
+++ reviews/views.py
@@ -8,20 +8,20 @@ from .forms import ReviewForm
 
 # Create your views here.
 @login_required
-def add_review(request):
+def add_review(request, product_id):
     """ Add a review to the store """
     if request.method == 'POST':
         form = ReviewForm(request.POST, request.FILES)
         if form.is_valid():
             review = form.save()
             messages.success(request, 'Successfully added review')
-            return redirect(reverse('product_detail', args=[product.id]))
+            return redirect(reverse('product_detail', args=[product_id]))
         else:
             messages.error(request, 'Failed to add review. Please ensure the form is valid')
     else:
         form = ReviewForm()
         
-    template = 'products/reviews.html'
+    template = 'products/products.html'
     context = {
         'form': form,
     }
@@ -30,16 +30,16 @@ def add_review(request):
 
 
 @login_required
-def edit_review(request, review_id):
+def edit_review(request, product_id):
     """ Edit a review in the store """
 
     review = get_object_or_404(Review)
     if request.method == 'POST':
-        form = ReviewForm(request.POST, request.FILES, instance=product)
+        form = ReviewForm(request.POST, request.FILES, instance=review)
         if form.is_valid():
             form.save()
             messages.success(request, 'Successfully updated review!')
-            return redirect(reverse('product_detail', args=[product.id]))
+            return redirect(reverse('product_detail', args=[review.id]))
         else:
             messages.error(request, 'Failed to update review. Please ensure the form is valid.')
     else:
