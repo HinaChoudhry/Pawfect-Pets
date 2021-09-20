e2f168ca23e0da239d2a82596a3c9f0cf0e30624
HinaChoudhry <hinachoudhry@msn.com>
2021-09-20 09:21:07 +0000

    Created forms.py for the reviews app and added the form python code

---
+++ reviews/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import Review
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ('title', 'comment',
+        )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders for the review form
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'title': 'Title',
+            'comment': 'Comment',
+        }
+
+        # Add placeholders and classes to input fields
+        self.fields['title'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
