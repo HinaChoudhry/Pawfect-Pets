8b8502a19355b430b23a746db34f3932062a1164
HinaChoudhry <hinachoudhry@msn.com>
2021-09-20 09:55:22 +0000

    Added to views.py in reviews - created an add review, edit review and delete review functions to allow users to add, edit or delete reviwds (delete review for super user only)

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+from .models import Review
+from .forms import ReviewForm
+
 
 # Create your views here.
+@login_required
+def add_review(request):
+    """ Add a review to the store """
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, request.FILES)
+        if form.is_valid():
+            review = form.save()
+            messages.success(request, 'Successfully added review')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to add review. Please ensure the form is valid')
+    else:
+        form = ReviewForm()
+        
+    template = 'products/reviews.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def edit_review(request, review_id):
+    """ Edit a review in the store """
+
+    review = get_object_or_404(Review)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated review!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update review. Please ensure the form is valid.')
+    else:
+        form = ReviewForm(instance=review)
+        messages.info(request, f'You are reviewing {product.name}')
+
+    template = 'products/edit_review.html'
+    context = {
+        'form': form,
+        'review': review,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_review(request, product_id):
+    """ Delete a product from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only superusers can do that')
+        return redirect(reverse('home'))
+        
+    review = get_object_or_404(Review)
+    review.delete()
+    messages.success(request, 'Review deleted!')
+    return redirect(reverse('products'))
