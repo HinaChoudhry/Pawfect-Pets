236cc152b310dbd2ce5ddd4f0d7aa485e31ce836
HinaChoudhry <hinachoudhry@msn.com>
2021-10-08 08:41:19 +0000

    Added Bootstrap modal for when deleting reviews

--- blog/static/blog/js/blog.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('delete-modal').on("click", function() {
-	document.querySelector('.comment-modal').style.display = "flex";
-});
-
-document.querySelector('.close').on("click", function() {
-	document.querySelector('.comment-modal').style.display = "none";
-});
