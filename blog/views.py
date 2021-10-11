f62461dd1ba5455ed6d595218a91001c8351e1fc
HinaChoudhry <hinachoudhry@msn.com>
2021-10-11 10:33:09 +0000

    Code refactoring in line with Pep8

--- blog/views.py
+++ blog/views.py
@@ -5,12 +5,11 @@ from .forms import BlogPostForm, BlogCommentForm
 from .models import BlogPost, BlogComment
 
 
-
 def blog(request):
     """ This view returns the blog page"""
     blogposts = BlogPost.objects.all().order_by("-date")
     form = BlogPostForm()
-    
+
     template = 'blog/blog.html'
     context = {
       'blogposts': blogposts,
@@ -41,7 +40,6 @@ def add_blog_post(request):
         messages.error(request, 'Sorry, only superusers can do that')
         return redirect(reverse('home'))
 
-    
     if request.method == 'POST':
         form = BlogPostForm(request.POST, request.FILES)
         if form.is_valid():
@@ -51,15 +49,17 @@ def add_blog_post(request):
             messages.success(request, 'Successfully added blog post')
             return redirect(reverse('blog'))
         else:
-            messages.error(request, 'Failed to add blog post. Please ensure the form is valid.')
+            messages.error(
+                request,
+                'Failed to add blog post. Please ensure the form is valid.')
     else:
         form = BlogPostForm()
-        
+
     template = 'blog/blog.html'
     context = {
-        'blogposts': blogposts,
+        'blogpost': blogpost,
         'form': form,
-       
+
     }
 
     return render(request, template, context)
@@ -78,7 +78,7 @@ def edit_blog_post(request, blogpost_id):
             form.save()
             messages.success(request, 'Successfully updated blog post!')
             return redirect('blog')
-            
+
         else:
             messages.error(
                     request,
@@ -100,7 +100,7 @@ def edit_blog_post(request, blogpost_id):
 @login_required
 def delete_blog_post(request, blogpost_id):
     """ Delete a review from the store """
-    
+
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only superusers can do that')
         return redirect(reverse('home'))
@@ -113,7 +113,7 @@ def delete_blog_post(request, blogpost_id):
 @login_required
 def add_blog_comment(request, blogpost_id):
     """ Add a blog comment """
-    
+
     blogpost = get_object_or_404(BlogPost, pk=blogpost_id)
     if request.method == 'POST':
         form = BlogCommentForm(request.POST, request.FILES)
@@ -121,17 +121,19 @@ def add_blog_comment(request, blogpost_id):
             blogcomment = form.save(commit=False)
             blogcomment.blogpost = blogpost
             blogcomment.user = request.user
-            blogcomment.save()        
+            blogcomment.save()
             messages.success(request, 'Successfully added comment')
             return redirect(reverse('blog_detail', args=[blogpost_id]))
         else:
-            messages.error(request, 'Failed to add blog comment. Please ensure the form is valid.')
+            messages.error(
+                request,
+                'Failed to add blog comment. Please ensure the form is valid.')
     else:
         form = BlogCommentForm()
-        
+
     template = 'blog_detail.html'
     context = {
-       
+
         'blogcomment': blogcomment,
         'form': form,
     }
@@ -148,25 +150,25 @@ def edit_blog_comment(request, blogcomment_id, blogpost_id):
 
         form = BlogCommentForm(request.POST, instance=blogcomment)
         if form.is_valid():
-           
 
             form.save()
             messages.success(request, 'Successfully updated comment!')
             return redirect(reverse('blog_detail', args=[blogpost.id]))
         else:
-            messages.error(request, 'Failed to update comment. Please ensure the form is valid.')
+            messages.error(
+                request,
+                'Failed to update comment. Please ensure the form is valid.')
     else:
         form = BlogCommentForm(instance=blogcomment)
-        messages.info(request, f'You are editing your comment for {blogpost.blog_title}')
+        messages.info(
+            request,
+            f'You are editing your comment for {blogpost.blog_title}')
 
-    template = 'edit_blog_comment.html'
     context = {
         'form': form,
         'blogcomment': blogcomment,
         'blogpost_id': blogpost_id,
         'blogcomment_id': blogcomment_id,
-        
-        
     }
 
     return render(request, 'blog/edit_blog_comment.html', context)
@@ -175,7 +177,7 @@ def edit_blog_comment(request, blogcomment_id, blogpost_id):
 @login_required
 def delete_blog_comment(request, blogcomment_id):
     """ Delete a review from the store """
-    
+
     blogcomment = get_object_or_404(BlogComment, pk=blogcomment_id)
     blogcomment.delete()
     messages.success(request, 'Blog comment deleted!')
