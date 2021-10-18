7284340b7ec6b9bd15f3d549685127a43c2cf7a5
HinaChoudhry <hinachoudhry@msn.com>
2021-10-18 08:46:57 +0000

    Amended new javascript files to be in line with Javascript validation rules

--- blog/static/blog/js/blog.js
+++ blog/static/blog/js/blog.js
@@ -2,26 +2,36 @@
 // exluding non-alphanumeric, the form is submitted. 
 
 let form = document.getElementById("blogcomment");
-
-let comment = document.getElementById("comment");
-
 function validateCode(event) {
-  event.preventDefault(); 
-  var comment = document.getElementById("id_comment").value;
-  console.log(comment);
-comment = comment.replace(/[^0-9a-z]/gi, '');
-
-if (comment.length > 0) { 
-    form.submit() 
-  }
-  else {
-  alert("Only Alphanumeric inputs are allowed");
-    
+    event.preventDefault(); 
+    var comment = document.getElementById("id_comment").value;
+    comment = comment.replace(/[^0-9a-z]/gi, '');
+    if (comment.length > 0) { 
+        form.submit();
+        }
+    else {
+        alert("Only Alphanumeric characters are allowed");
+      }
     }
-  }
-  form.addEventListener("submit", validateCode);
+form.addEventListener("submit", validateCode);
 
  
+// Targets alphanumeric characters and if the length of the string is greater 0, 
+// exluding non-alphanumeric, the form is submitted. 
+
+function validateCode(event) {
+    event.preventDefault(); 
+    var comment = document.getElementById("id_blog_content").value;
+    comment = comment.replace(/[^0-9a-z]/gi, '');
+    if (comment.length > 0) { 
+        form.submit();
+        }
+    else {
+        alert("Only Alphanumeric characters are allowed");
+      }
+    }
+form.addEventListener("submit", validateCode);
 
 
-  
+
+
--- reviews/static/reviews/js/reviews.js
+++ reviews/static/reviews/js/reviews.js
@@ -2,45 +2,37 @@
 // exluding non-alphanumeric, the form is submitted. 
 
 let form = document.getElementById("reviewcomment");
-let comment = document.getElementById("comment");
 
-console.log("outside function")
+console.log("outside function");
 function validateCode(event) {
-  event.preventDefault(); 
-  var comment = document.getElementById("id_comment").value;
-  console.log(comment);
-comment = comment.replace(/[^0-9a-z]/gi, '');
-
-if (comment.length > 0) { 
-    form.submit() 
-  }
-  else {
-  alert("Only Alphanumeric inputs are allowed");
-    
+    event.preventDefault(); 
+    var comment = document.getElementById("id_comment").value;
+    console.log(comment);
+    comment = comment.replace(/[^0-9a-z]/gi, '');
+    if (comment.length > 0) { 
+        form.submit();
+      }
+    else {
+        alert("Only Alphanumeric characters are allowed");
+      }
     }
-  }
-  form.addEventListener("submit", validateCode);
+form.addEventListener("submit", validateCode);
 
 // Targets alphanumeric characters and if the length of the string is greater 0, 
 // exluding non-alphanumeric, the form is submitted. 
 
-  let title = document.getElementById("title");
- 
-  function validateCode(event) {
+function validateCode(event) {
     event.preventDefault(); 
     var title = document.getElementById("id_title").value;
     console.log(title);
-  title = title.replace(/[^0-9a-z]/gi, '');
-  
-  if (title.length > 0) { 
-      form.submit() 
-    }
+    title = title.replace(/[^0-9a-z]/gi, '');
+    if (title.length > 0) { 
+        form.submit();
+      }
     else {
-    alert("Only Alphanumeric inputs are allowed");
-      
+        alert("Only Alphanumeric characters are allowed");
       }
     }
-    form.addEventListener("submit", validateCode);
-  
+  form.addEventListener("submit", validateCode);
 
   
