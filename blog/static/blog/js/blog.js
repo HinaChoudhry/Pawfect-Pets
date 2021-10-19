fec9613ebc60cc9cf504c887c365699ffde21d45
HinaChoudhry <hinachoudhry@msn.com>
2021-10-19 09:57:03 +0000

    Final check over files

--- blog/static/blog/js/blog.js
+++ blog/static/blog/js/blog.js
@@ -15,23 +15,5 @@ function validateCode(event) {
     }
 form.addEventListener("submit", validateCode);
 
- 
-// Targets alphanumeric characters and if the length of the string is greater 0, 
-// exluding non-alphanumeric, the form is submitted. 
-
-function validateCode(event) {
-    event.preventDefault(); 
-    var comment = document.getElementById("id_blog_content").value;
-    comment = comment.replace(/[^0-9a-z]/gi, '');
-    if (comment.length > 0) { 
-        form.submit();
-        }
-    else {
-        alert("Only Alphanumeric characters are allowed");
-      }
-    }
-form.addEventListener("submit", validateCode);
-
-
 
 
